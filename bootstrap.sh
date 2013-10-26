280a3b317f5eef502cd4f81580661da25efbad43
Nezar Abdennur <nabdennur@gmail.com>
2013-10-26 04:25:23 -0400

    uncommitting bootstrap.sh

--- bootstrap.sh
+++
@@ -1,4 +0,0 @@
-heroku config:set LIBRARY_PATH=/app/.heroku/vendor/lib  --app $1
-heroku config:set LD_LIBRARY_PATH=/app/.heroku/vendor/lib  --app $1
-heroku config:set PATH=bin:app/.heroku/venv/bin:/bin:/usr/local/bin:/usr/bin --app $1
-heroku config:set BUILDPACK_URL=https://github.com/ddollar/heroku-buildpack-multi.git --app $1
