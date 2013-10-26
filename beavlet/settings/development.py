7e9e08d35974054677200961454df3bd98a23f0b
Nezar Abdennur <nabdennur@gmail.com>
2013-10-26 02:15:33 -0400

    move common settings to __init__

--- beavlet/settings/development.py
+++ beavlet/settings/development.py
@@ -1,6 +1,6 @@
 import os
 
-from beavlet.settings.common import *
+from beavlet.settings import *
 
 #==============================================================================
 # Generic Django project settings
--- beavlet/settings/production.py
+++ beavlet/settings/production.py
@@ -1,13 +1,13 @@
 import os
 import dj_database_url
 
-from beavlet.settings.common import *
+from beavlet.settings import *
 
 #==============================================================================
 # Generic Django project settings
 #==============================================================================
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 if not os.environ.has_key('DATABASE_URL'):
@@ -24,6 +24,7 @@ if not DEBUG:
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    #AWS_PRELOAD_METADATA = True # necessary to fix manage.py collectstatic command to only upload changed files instead of all files
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 
