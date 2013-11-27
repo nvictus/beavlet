e59de9442b79ff3f3ea65e201bbf6f5dfdac4ab3
Nezar Abdennur <nabdennur@gmail.com>
2013-11-27 17:32:29 -0500

    source this to set environment vars

---
+++ bootstrap.sh
@@ -0,0 +1,26 @@
+# Source this file to set up your development environment variables
+
+# Django debug setting
+export DJ_DEBUG=True
+
+# Allowed host
+export HOSTNAME='*'
+
+# Settings file to use
+export ENVIRONMENT=
+
+# Django security
+export SECRET_KEY=
+
+# Django storages static file serving
+export USE_AWS=False
+export AWS_STORAGE_BUCKET_NAME=
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+
+# Django database url in production mode
+export DATABASE_URL= #sqlite:///dev.sqlite3
+
+# Dropbox API
+export DROPBOX_APP_KEY=
+export DROPBOX_APP_SECRET=
